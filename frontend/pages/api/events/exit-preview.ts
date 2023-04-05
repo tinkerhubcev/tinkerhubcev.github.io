e78e4456d69ffbe68c3eaedc070f1fc7612655f2
adhilroshan <adhilchra@gmail.com>
2023-04-05 23:04:43 +0530

    Initial Commit

--- frontend/pages/api/events/exit-preview.ts
+++ frontend/pages/api/events/exit-preview.ts
@@ -1,6 +1,12 @@
-
-export default function exit(req: any, res: { clearPreviewData: () => void; writeHead: (arg0: number, arg1: { Location: string }) => void; end: () => void }) {
-  res.clearPreviewData()
-  res.writeHead(307, {Location: '/events'})
-  res.end()
+export default function exit(
+  req: any,
+  res: {
+    clearPreviewData: () => void;
+    writeHead: (arg0: number, arg1: { Location: string }) => void;
+    end: () => void;
+  }
+) {
+  res.clearPreviewData();
+  res.writeHead(307, { Location: "/events" });
+  res.end();
 }
--- frontend/pages/api/events/preview.ts
+++ frontend/pages/api/events/preview.ts
@@ -1,4 +1,11 @@
-export default function preview(req: any, res: { setPreviewData: (arg0: {}) => void; writeHead: (arg0: number, arg1: { Location: string; }) => void; end: () => void; }) {
+export default function preview(
+  req: any,
+  res: {
+    setPreviewData: (arg0: {}) => void;
+    writeHead: (arg0: number, arg1: { Location: string }) => void;
+    end: () => void;
+  }
+) {
   res.setPreviewData({});
   res.writeHead(307, { Location: "/events" });
   res.end();
